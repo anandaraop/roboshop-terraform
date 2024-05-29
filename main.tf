6b54e6d9ffc8a8fd6a834c9b59a38c536f3e4b01
SVR Kalluru <raghuk.vit@gmail.com>
2024-05-29 06:50:55 +0530

    roboshop terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 variable "components" {
-  default = ["frontend", "catalogue", "cart", "user", "shipping", "payment", "dispatch" ]
+  default = ["frontend", "catalogue", "cart", "user", "shipping", "payment", "dispatch"]
 }
 
 resource "aws_instance" "instance" {
@@ -21,3 +21,18 @@ resource "aws_route53_record" "record" {
   ttl     = 3
 }
 
+
+module "vpc" {
+  source = "./modules/vpc"
+
+  availability_zones     = var.availability_zones
+  backend_subnets        = var.backend_subnets
+  db_subnets             = var.db_subnets
+  default_route_table_id = var.default_route_table_id
+  default_vpc_cidr       = var.default_vpc_cidr
+  default_vpc_id         = var.default_vpc_id
+  env                    = var.env
+  frontend_subnets       = var.frontend_subnets
+  public_subnets         = var.public_subnets
+  vpc_cidr_block         = var.vpc_cidr_block
+}
