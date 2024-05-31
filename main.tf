bb4b7ca34c55940893d90bb297e481b312258895
SVR Kalluru <raghuk.vit@gmail.com>
2024-05-31 06:39:54 +0530

    Ec2 to centos8

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ variable "components" {
 
 resource "aws_instance" "instance" {
   count                  = length(var.components)
-  ami                    = "ami-0d035b0fa9820ac92"
+  ami                    = "ami-0f3c7d07486cad139"
   instance_type          = "t3.small"
   subnet_id              = module.vpc.backend_subnets[0]
   tags = {
